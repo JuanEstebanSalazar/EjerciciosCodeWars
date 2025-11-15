d0f9a646d3cf29db395d59585ecebfebcbc0dbe0
Juan Esteban Salazar Londoño <juanesteban.salazar1@utp.edu.co>
2025-11-15 14:54:18 -0500

    Cambio de logica BitCounting Codewars

--- src/Ejercicios/BitCounting.java
+++ src/Ejercicios/BitCounting.java
@@ -1,10 +1,22 @@
 package Ejercicios;
 
+/* Descripción:
+
+Escriba una función que tome un entero como entrada, y devuelve el número de bits que son iguales
+a uno en la representación binaria de ese número. Puede garantizar que la entrada no es negativa.
+
+Ejemplo: La representación binaria de 1234 Es 10011010010,
+por lo que la función debe volver 5 En este caso
+ */
 
 public class BitCounting {
 
     public static int countBits(int n) {
 
+        // Evitar que ingresen un numero entero negativo
+        if(n<0){
+            throw new IllegalArgumentException("El método countBits solo acepta números enteros no negativos (n >= 0). Valor ingresado: " + n);
+        }
 
         int cociente = 0;
         int bits = 0;
